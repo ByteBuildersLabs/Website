cd938a50d793062a3dab7f1448cdf4741fad0f83
PoulavBhowmick03 <bpoulav@gmail.com>
2024-10-29 00:37:14 +0530

    made the changes, and fixed the issue

--- src/components/NFTCollection/index.tsx
+++ src/components/NFTCollection/index.tsx
@@ -1,4 +1,4 @@
-import { Link, useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import LuisImg from '../../assets/img/Luis.png';
 import Daniel from '../../assets/img/daniel.jpeg';
 import Marco from '../../assets/img/marco.jpeg';
@@ -7,102 +7,49 @@ import './main.css';
 
 interface NftMetaData {
   name: string;
-  description: string;
   image: string;
   tokenId: number;
-  owner: string;
 }
 
 export default function Component() {
-  const { collectionName } = useParams<{ collectionName: string }>();
 
-  // Dummy data for the collection
   const animals: NftMetaData[] = [
-    {
-      name: "Mystic Animals #1",
-      description: "Mystic Animals is a collection that is only owned by the most fearless investors",
-      image: LuisImg,
-      tokenId: 1,
-      owner: "0xdAC17F958D2ee523a2206206994597C13D831ec7"
-    },
-    {
-      name: "Mystic Animals #2",
-      description: "Mystic Animals is a collection that is only owned by the most fearless investors",
-      image: Daniel,
-      tokenId: 2,
-      owner: "0x742d35Cc6634C0532925a3b844Bc454e4438f44e"
-    },
-    {
-      name: "Mystic Animals #3",
-      description: "Mystic Animals is a collection that is only owned by the most fearless investors",
-      image: Rol,
-      tokenId: 3,
-      owner: "0xdAC17F958D2ee523a2206206994597C13D831ec7"
-    },
-    {
-      name: "Mystic Animals #4",
-      description: "Mystic Animals is a collection that is only owned by the most fearless investors",
-      image: Marco,
-      tokenId: 4,
-      owner: "0x123d35Cc6634C0532925a3b844Bc454e4438f123"
-    },
-    {
-      name: "Mystic Animals #5",
-      description: "Mystic Animals is a collection that is only owned by the most fearless investors",
-      image: Rol,
-      tokenId: 5,
-      owner: "0xdAC17F958D2ee523a2206206994597C13D831ec7"
-    },
-    {
-      name: "Mystic Animals #6",
-      description: "Mystic Animals is a collection that is only owned by the most fearless investors",
-      image: Daniel,
-      tokenId: 6,
-      owner: "0x742d35Cc6634C0532925a3b844Bc454e4438f44e"
-    },
-    {
-      name: "Mystic Animals #7",
-      description: "Mystic Animals is a collection that is only owned by the most fearless investors",
-      image: Marco,
-      tokenId: 7,
-      owner: "0x123d35Cc6634C0532925a3b844Bc454e4438f123"
-    },
-    {
-      name: "Mystic Animals #8",
-      description: "Mystic Animals is a collection that is only owned by the most fearless investors",
-      image: LuisImg,
-      tokenId: 8,
-      owner: "0xdAC17F958D2ee523a2206206994597C13D831ec7"
-    },
-    {
-      name: "Mystic Animals #9",
-      description: "Mystic Animals is a collection that is only owned by the most fearless investors",
-      image: Daniel,
-      tokenId: 9,
-      owner: "0x742d35Cc6634C0532925a3b844Bc454e4438f44e"
-    }
+    { name: "Mystic Animals #1", image: LuisImg, tokenId: 1 },
+    { name: "Mystic Animals #2", image: Daniel, tokenId: 2 },
+    { name: "Mystic Animals #3", image: Rol, tokenId: 3 },
+    { name: "Mystic Animals #4", image: Marco, tokenId: 4 },
+    { name: "Mystic Animals #5", image: Rol, tokenId: 5 },
+    { name: "Mystic Animals #6", image: Daniel, tokenId: 6 },
+    { name: "Mystic Animals #7", image: Marco, tokenId: 7 },
+    { name: "Mystic Animals #8", image: LuisImg, tokenId: 8 },
+    { name: "Mystic Animals #9", image: Daniel, tokenId: 9 }
   ];
 
   return (
-    <div className="nft-room">
-      <div className="container">
-        <div className="section-title text-center my-4">
-          <h1 className="display-5">Mystic Animals Collection</h1>
-          <span className="text-muted">A collection for the most fearless investors</span>
-          <h2 className="collection-name text-primary">{collectionName}</h2>
-        </div>
-        <div className="separator"></div>
-        <div className="row">
-          {animals.map(({ name, image, tokenId }) => (
-            <div key={tokenId} className="col-6 col-md-4 col-lg-3 mb-4">
-              <Link to={`/nftroom/${collectionName}/${tokenId}`} className="nft-card d-block text-center p-1">
-                <img src={image} alt={name} className="img-fluid" />
-                <p className="mt-2">{name}</p>
-              </Link>
+    <>
+      {
+        <div className="nft-room">
+          <div className={"nft-owner"}>
+            <div className="section-title title-style-two text-center mb-60">
+              <span>Collectibles</span>
+              <h2>
+                Holder <span className="d-block">0xdAC...1ec7</span>
+              </h2>
             </div>
-          ))}
+            <div className="collection row">
+              {animals.map(({ name, image, tokenId }: NftMetaData) => {
+                return (
+                  <Link key={tokenId} to={`/nftroom/`} className="col-4 yellow">
+                    <div className="nft-card">
+                      <img src={image} alt="" />
+                      <p>{name}</p>
+                    </div>
+                  </Link>
+                );
+              })}
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-  );
-}
+      }
+    </>
+  );}
