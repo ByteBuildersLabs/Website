2c24e34976f4573e8be67df9bb0b2fc8e945a5ad
Marco Araya Jiménez <marcojime23@gmail.com>
2024-10-29 02:16:08 -0600

    feat: add basic structure to add theme for controller

---
+++ src/theme/presets.ts
@@ -0,0 +1,21 @@
+/*
+
+Example of a preset to include our theme in the controller
+We only need to add this code and files (icon.png, cover.png) to the folder /whitelabel/bytebeasts
+
+Review old PRs: https://github.com/cartridge-gg/controller/pull/421/files
+
+import { ControllerThemePresets } from "./types";
+
+export const defaultPresets: ControllerThemePresets = {
+  "bytebeasts": {
+    id: "bytebeasts",
+    name: "Bytebeasts",
+    icon: "/whitelabel/bytebeasts/icon.png",
+    cover: "/whitelabel/bytebeasts/cover.png",
+    colors: {
+      primary: "#ff3333",
+    },
+  },
+
+*/
